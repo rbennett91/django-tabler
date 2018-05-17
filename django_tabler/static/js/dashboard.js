8512a4d24e2671cb76aa120d07e417a8d4771ef2
ryan bennett <ryan.bennett@outlook.com>
2018-05-17 14:22:20 -0400

    rename assets -> static. update dashboard.js to use django static loading

--- django_tabler/static/js/dashboard.js
+++ django_tabler/static/js/dashboard.js
@@ -9,16 +9,16 @@ shim: {
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
-'core': 'assets/js/core',
-'jquery': 'assets/js/vendors/jquery-3.2.1.min',
-'bootstrap': 'assets/js/vendors/bootstrap.bundle.min',
-'sparkline': 'assets/js/vendors/jquery.sparkline.min',
-'selectize': 'assets/js/vendors/selectize.min',
-'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
-'vector-map': 'assets/js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': 'assets/js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': 'assets/js/vendors/jquery-jvectormap-world-mill',
-'circle-progress': 'assets/js/vendors/circle-progress.min',
+'core': 'static/js/core',
+'jquery': '{% static 'js/vendors/jquery-3.2.1.min' %}',
+'bootstrap': '{% static 'js/vendors/bootstrap.bundle.min' %}',
+'sparkline': '{% static 'js/vendors/jquery.sparkline.min' %}',
+'selectize': '{% static 'js/vendors/selectize.min' %}',
+'tablesorter': '{% static 'js/vendors/jquery.tablesorter.min' %}',
+'vector-map': '{% static  'js/vendors/jquery-jvectormap-2.0.3.min' %}',
+'vector-map-de': '{% static 'js/vendors/jquery-jvectormap-de-merc' %}',
+'vector-map-world': '{% static 'js/vendors/jquery-jvectormap-world-mill' %}',
+'circle-progress': '{% static 'js/vendors/circle-progress.min' %}',
 }
 });
 window.tabler = {
